cf00f9d50d399eeba3f476d0d368619faa9cde43
Lexxy10101 <136014685+Lexxy10101@users.noreply.github.com>
2026-01-29 13:00:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//Creating specific eveniments which will apply when an element is it collapsed, in my case the menu which is it completely closed or opened
+document.addEventListener('DOMContentLoaded', function(){
+    var navbarCollapse = document.getElementById('navbarNavAltMarkup');
+    var body = document.body;
+    if(navbarCollapse){
+        navbarCollapse.addEventListener('shown.bs.collapse', function(){
+            body.classList.add('navbar-open');
+        });
+        navbarCollapse.addEventListener('hidden.bs.collapse',function(){
+            body.classList.remove('navbar-open');
+        });
+    }
+});
+
+
+const modificare = document.querySelector('.hero-content h2');
+modificare.textContent = 'Acesta este un titlu modificat din JavaScript';
+
+
